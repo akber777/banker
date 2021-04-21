2669670340f979962c3f8e501dbbb8415c01930d
akber <akberakhmedzadeh@gmail.com>
2021-04-22 02:58:33 +0400

    'fallback'

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ const path = require("path");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "build/static/js/4.23078a0a.chunk.js"),
+  context:"__dirname",
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "afterCompres.js",
-    chunkFilename: "[name].js",
+  },
+  devServer: {
+    historyApiFallback: true
   },
   optimization: {
     splitChunks: {
